b90d2bc4def902d247b84d2e0f068ee04adc943b
Milos Vasic <i@mvasic.ru>
2023-12-20 18:23:25 +0300

    Check if credentials are really equal.

--- Main/src/main/java/com/redelf/commons/Credentials.kt
+++ Main/src/main/java/com/redelf/commons/Credentials.kt
@@ -14,4 +14,21 @@ data class Credentials(
 ) {
 
     fun isEmpty(): Boolean = TextUtils.isEmpty(username) || TextUtils.isEmpty(password)
+    override fun equals(other: Any?): Boolean {
+
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Credentials
+
+        if (username != other.username) return false
+        return password == other.password
+    }
+
+    override fun hashCode(): Int {
+
+        var result = username.hashCode()
+        result = 31 * result + password.hashCode()
+        return result
+    }
 }
