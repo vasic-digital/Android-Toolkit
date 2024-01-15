5573d7e18844f74aacab3f0441ba0c8492da3c3c
Милош Васић <i@mvasic.ru>
2024-01-15 18:14:22 +0300

    Alarm scheduling.

---
+++ Main/src/main/java/com/redelf/commons/scheduling/alarm/AlarmReceiver.kt
@@ -0,0 +1,17 @@
+package com.redelf.commons.scheduling.alarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import timber.log.Timber
+
+class AlarmReceiver(private val callback: AlarmCallback) : BroadcastReceiver() {
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+        Timber.v("Alarm received: $intent")
+
+        // TODO: GEt extra from intent and pass to callback
+        callback.onAlarm(-1)
+    }
+}
---
+++ Main/src/main/java/com/redelf/commons/scheduling/alarm/AlarmScheduler.kt
@@ -0,0 +1,86 @@
+package com.redelf.commons.scheduling.alarm
+
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import com.redelf.commons.scheduling.Schedule
+import timber.log.Timber
+
+class AlarmScheduler(
+
+    ctx: Context,
+    private val logTag: String = "Alarm scheduling ::"
+
+) : Schedule<Int> {
+
+    companion object {
+
+        const val ALARM_ACTION = "AlarmScheduler.ON_ALARM"
+    }
+
+    private val context = ctx.applicationContext
+
+    private val alarmIntent = Intent(context, AlarmReceiver::class.java)
+
+    private val flags = PendingIntent.FLAG_UPDATE_CURRENT or
+            PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+
+    private val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+
+    init {
+
+        alarmIntent.action = ALARM_ACTION
+    }
+    override fun schedule(
+
+        what: Int,
+        toWhen: Long
+
+    ): Boolean {
+
+        val tag = "$logTag ON ::"
+
+        val pendingIntent = PendingIntent.getBroadcast(
+
+            context,
+            what,
+            alarmIntent,
+            flags
+        )
+
+        unSchedule(what)
+
+        Timber.w("$tag Scheduling new alarm")
+
+        alarmManager.setExactAndAllowWhileIdle(
+
+            AlarmManager.RTC_WAKEUP,
+            toWhen,
+            pendingIntent
+        )
+
+        return true
+    }
+
+    override fun unSchedule(what: Int): Boolean {
+
+        val tag = "$logTag OFF ::"
+
+        val pendingIntent = PendingIntent.getBroadcast(
+
+            context,
+            what,
+            alarmIntent,
+            flags
+        )
+
+        Timber.v("$tag Cancelling scheduled alarm (if any)")
+
+        alarmManager.cancel(pendingIntent)
+
+        Timber.v("$tag COMPLETED")
+
+        return true
+    }
+}
