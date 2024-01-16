ed9d96e7cbcc0e96bf9d18275ef71ed6d8877e35
Милош Васић <i@mvasic.ru>
2024-01-16 11:39:14 +0300

    Alarm receiver registration.

--- Main/src/main/java/com/redelf/commons/scheduling/alarm/AlarmReceiver.kt
+++ Main/src/main/java/com/redelf/commons/scheduling/alarm/AlarmReceiver.kt
@@ -3,15 +3,54 @@ package com.redelf.commons.scheduling.alarm
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.redelf.commons.Registration
+import com.redelf.commons.callback.CallbackOperation
+import com.redelf.commons.callback.Callbacks
 import timber.log.Timber
 
-class AlarmReceiver(private val callback: AlarmCallback) : BroadcastReceiver() {
+class AlarmReceiver : BroadcastReceiver() {
+
+    companion object : Registration<AlarmCallback> {
+
+        private val callbacks: Callbacks<AlarmCallback> = Callbacks("AlarmReceiver")
+
+        override fun register(subscriber: AlarmCallback) {
+
+            callbacks.register(subscriber)
+        }
+
+        override fun unregister(subscriber: AlarmCallback) {
+
+            callbacks.unregister(subscriber)
+        }
+
+        override fun isRegistered(subscriber: AlarmCallback): Boolean {
+
+            return callbacks.isRegistered(subscriber)
+        }
+
+        private fun onAlarmReceived(value: Int) {
+
+            callbacks.doOnAll(
+
+                operation = object : CallbackOperation<AlarmCallback> {
+
+                    override fun perform(callback: AlarmCallback) {
+
+                        callback.onAlarm(value)
+                    }
+                },
+
+                name = "onAlarmReceived: $value"
+            )
+        }
+    }
 
     override fun onReceive(context: Context?, intent: Intent?) {
 
         Timber.v("Alarm received: $intent")
 
         // TODO: Get extra from intent and pass to callback
-        callback.onAlarm(-1)
+        onAlarmReceived(-1)
     }
 }
