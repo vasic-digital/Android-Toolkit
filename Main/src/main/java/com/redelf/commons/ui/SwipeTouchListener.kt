46dd8bdc06411960de11b96f55ed1b343dd9cc00
Milos Vasic <i@mvasic.ru>
2024-02-10 13:38:28 +0100

    Swipe improvements.

--- Main/src/main/java/com/redelf/commons/ui/SwipeTouchListener.kt
+++ Main/src/main/java/com/redelf/commons/ui/SwipeTouchListener.kt
@@ -5,27 +5,42 @@ import android.annotation.SuppressLint
 import android.graphics.Rect
 import android.view.MotionEvent
 import android.view.View
+import timber.log.Timber
 
 class SwipeTouchListener(private val swipeView: View) : View.OnTouchListener {
 
     interface SwipeListener {
+
         fun onDragStart()
+
         fun onDragStop()
+
         fun onDismissed()
     }
 
     private var tracking = false
     private var startY: Float = 0.0f
-    private var swipeListener: SwipeListener? = null
     private var isDragStarted = false
+    private var swipeListener: SwipeListener? = null
 
-    fun setSwipeListener(swipeListener: SwipeListener) {
+    fun setSwipeListener(swipeListener: SwipeListener?) {
 
         this.swipeListener = swipeListener
+
+        if (swipeListener == null) {
+
+            swipeView.setOnTouchListener(null)
+
+        } else {
+
+            swipeView.setOnTouchListener(this)
+        }
     }
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+
+        Timber.v("On touch: $event")
 
         event?.let {
 
@@ -49,7 +64,8 @@ class SwipeTouchListener(private val swipeView: View) : View.OnTouchListener {
                 MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
 
                     tracking = false
-                    animateSwipeView(v!!.height)
+
+                    animateSwipeView()
 
                     return true
                 }
@@ -76,24 +92,32 @@ class SwipeTouchListener(private val swipeView: View) : View.OnTouchListener {
                 }
             }
         }
+
         return false
     }
 
-    private fun animateSwipeView(parentHeight: Int) {
+    private fun animateSwipeView() { //parentHeight: Int
 
-        val halfHeight = parentHeight / 2
+//        val halfHeight = parentHeight / 2
         val currentPosition = swipeView.translationY
         var animateTo = 0.0f
-        if (currentPosition < -halfHeight) {
-            animateTo = (-parentHeight).toFloat()
-        } else if (currentPosition > halfHeight) {
-            animateTo = parentHeight.toFloat()
-        }
+
+//        if (currentPosition < -halfHeight) {
+//
+//            animateTo = (-parentHeight).toFloat()
+//
+//        } else if (currentPosition > halfHeight) {
+//
+//            animateTo = parentHeight.toFloat()
+//        }
 
         if (animateTo == 0.0f) {
+
             swipeListener?.onDragStop()
             isDragStarted = false
+
         } else {
+
             swipeListener?.onDismissed()
         }
 
