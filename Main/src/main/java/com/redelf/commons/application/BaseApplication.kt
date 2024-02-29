92a5f9770cba972f5a9b0c217ec285c88b9c077a
Milos Vasic <i@mvasic.ru>
2024-02-29 13:59:55 +0300

    Resolving the Strict Mode warnings.

--- Main/src/main/java/com/redelf/commons/application/BaseApplication.kt
+++ Main/src/main/java/com/redelf/commons/application/BaseApplication.kt
@@ -203,50 +203,6 @@ abstract class BaseApplication :
         managers.addAll(populateManagers())
         defaultManagerResources.putAll(populateDefaultManagerResources())
 
-        Timber.i("Data: Initializing")
-
-        val salter = object : Salter {
-
-            override fun getSalt(): String {
-
-                var salt = takeSalt()
-
-                if (salt.length > 5) {
-
-                    salt = salt.substring(0, 5)
-                }
-
-                Timber.v("Get salt :: Length: ${salt.length}")
-
-                return salt
-            }
-        }
-
-        val getParser = object : Obtain<Parser?> {
-
-            override fun obtain(): Parser {
-
-                return GsonParser(
-
-                    object : Obtain<Gson> {
-
-                        override fun obtain(): Gson {
-
-                            return GsonBuilder()
-                                .enableComplexMapKeySerialization()
-                                .create()
-                        }
-                    }
-                )
-            }
-        }
-
-        Data.init(applicationContext, getString(R.string.app_name), salter)
-            .setParser(getParser)
-            .build()
-
-        Timber.i("Data: Initialized")
-
         doCreate()
     }
 
@@ -255,6 +211,50 @@ abstract class BaseApplication :
         try {
 
             exec {
+
+                Timber.i("Data: Initializing")
+
+                val salter = object : Salter {
+
+                    override fun getSalt(): String {
+
+                        var salt = takeSalt()
+
+                        if (salt.length > 5) {
+
+                            salt = salt.substring(0, 5)
+                        }
+
+                        Timber.v("Get salt :: Length: ${salt.length}")
+
+                        return salt
+                    }
+                }
+
+                val getParser = object : Obtain<Parser?> {
+
+                    override fun obtain(): Parser {
+
+                        return GsonParser(
+
+                            object : Obtain<Gson> {
+
+                                override fun obtain(): Gson {
+
+                                    return GsonBuilder()
+                                        .enableComplexMapKeySerialization()
+                                        .create()
+                                }
+                            }
+                        )
+                    }
+                }
+
+                Data.init(applicationContext, getString(R.string.app_name), salter)
+                    .setParser(getParser)
+                    .build()
+
+                Timber.i("Data: Initialized")
 
                 onDoCreate()
 
