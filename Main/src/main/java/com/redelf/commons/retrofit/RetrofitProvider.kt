878d1bb12fa76b5df6bc297e95465ac961529965
Milos Vasic <i@mvasic.ru>
2024-04-25 17:47:53 +0300

    Reduced the pool size.

--- Main/src/main/java/com/redelf/commons/retrofit/RetrofitProvider.kt
+++ Main/src/main/java/com/redelf/commons/retrofit/RetrofitProvider.kt
@@ -74,7 +74,7 @@ object RetrofitProvider : ObtainParametrized<Retrofit, RetrofitApiParameters> {
 
         val pool = ConnectionPool(
 
-            maxIdleConnections = 10,
+            maxIdleConnections = 5,
             keepAliveDuration = 5,
             timeUnit = TimeUnit.MINUTES
         )
