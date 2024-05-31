836eaedc6b59f5bacbc97e7079524c31381583c7
Milos Vasic <i@mvasic.ru>
2024-05-31 18:03:07 +0300

    Migration base.

---
+++ Main/src/main/java/com/redelf/commons/migration/DataMigration.kt
@@ -0,0 +1,66 @@
+package com.redelf.commons.migration
+
+import com.redelf.commons.obtain.OnObtain
+import timber.log.Timber
+
+abstract class DataMigration<SOURCE, TARGET> {
+
+    abstract val id: Long
+
+    abstract fun getSource(callback: OnObtain<SOURCE>)
+
+    abstract fun getTarget(source: SOURCE, callback: OnObtain<TARGET>)
+
+    fun migrate(callback: OnObtain<Boolean>) {
+
+        val tag = "Migrate :: $id ::"
+
+        Timber.v("$tag START")
+
+        val onTarget = object : OnObtain<TARGET> {
+
+            override fun onCompleted(data: TARGET) {
+
+                Timber.v("$tag Target obtained: $data")
+
+                apply(data, callback)
+            }
+
+            override fun onFailure(error: Throwable) {
+
+                callback.onFailure(error)
+            }
+        }
+
+        val onSource = object : OnObtain<SOURCE> {
+
+            override fun onCompleted(data: SOURCE) {
+
+                Timber.v("$tag Source obtained: $data")
+
+                data?.let {
+
+                    Timber.v("$tag Get target")
+
+                    getTarget(data, onTarget)
+                }
+
+                if (data == null) {
+
+                    callback.onCompleted(true)
+                }
+            }
+
+            override fun onFailure(error: Throwable) {
+
+                callback.onFailure(error)
+            }
+        }
+
+        Timber.v("$tag Get source")
+
+        getSource(onSource)
+    }
+
+    abstract fun apply(target: TARGET, callback: OnObtain<Boolean>)
+}
