c31f3e2c73d45425cb16771163cc8633af549941
Milos Vasic <i@mvasic.ru>
2024-07-15 21:57:00 +0300

    More asserting.

--- Main/src/androidTest/java/com/redelf/commons/test/HttpProxiesTest.kt
+++ Main/src/androidTest/java/com/redelf/commons/test/HttpProxiesTest.kt
@@ -3,6 +3,7 @@ package com.redelf.commons.test
 import com.redelf.commons.proxy.http.HttpProxies
 import org.junit.Assert
 import org.junit.Test
+import java.util.concurrent.atomic.AtomicLong
 
 class HttpProxiesTest : BaseTest() {
 
@@ -16,6 +17,25 @@ class HttpProxiesTest : BaseTest() {
 
             Assert.assertNotNull(obtained)
             Assert.assertTrue(obtained.isNotEmpty())
+
+            val iterator = obtained.iterator()
+            val quality = AtomicLong(Long.MAX_VALUE)
+
+            while (iterator.hasNext()) {
+
+                val proxy = iterator.next()
+
+                Assert.assertNotNull(proxy)
+                Assert.assertTrue(proxy.address.isNotBlank())
+                Assert.assertTrue(proxy.port > 0)
+                Assert.assertTrue(proxy.isAlive(applicationContext))
+
+                val newQuality = proxy.getQuality()
+
+                Assert.assertTrue(newQuality < quality.get())
+
+                quality.set(newQuality)
+            }
 
         } catch (e: Exception) {
 
