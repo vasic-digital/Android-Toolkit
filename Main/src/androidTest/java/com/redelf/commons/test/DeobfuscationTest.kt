162c7d13f6fad6f6436517fd15968ff0307caaf0
Milos Vasic <i@mvasic.ru>
2024-07-22 19:37:33 +0300

    Test obfuscated string added.

--- Main/src/androidTest/java/com/redelf/commons/test/DeobfuscationTest.kt
+++ Main/src/androidTest/java/com/redelf/commons/test/DeobfuscationTest.kt
@@ -1,11 +1,16 @@
 package com.redelf.commons.test
 
+import org.junit.Assert
 import org.junit.Test
 
 class DeobfuscationTest : BaseTest() {
 
     @Test
     fun testDeobfuscation() {
+
+        val obfuscated = applicationContext.getString(com.redelf.commons.R.string.ob_test)
+
+        Assert.assertTrue(obfuscated.isNotEmpty())
 
         // TODO: Implement deobfuscation test
     }
