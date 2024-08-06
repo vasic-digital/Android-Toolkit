4205b0b7f16630fd407a36c8103d1c354f651815
Milos Vasic <i@mvasic.ru>
2024-08-06 06:10:20 +0300

    The Echo module - First touches.

--- Echo/src/main/java/com/redelf/commons/interprocess/echo/EchoWorker.kt
+++ Echo/src/main/java/com/redelf/commons/interprocess/echo/EchoWorker.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import androidx.work.WorkerParameters
 import com.redelf.commons.interprocess.InterprocessWorker
 
-class EchoService(ctx: Context, params: WorkerParameters) : InterprocessWorker(ctx, params) {
+class EchoWorker(ctx: Context, params: WorkerParameters) : InterprocessWorker(ctx, params) {
 
     override fun doWork(): Result {
 
