764cc976a372e7488710952b088a43d7d9b3e1d8
Milos Vasic <i@mvasic.ru>
2024-08-13 21:59:02 +0300

    Echo application.

--- Echo/src/main/java/com/redelf/commons/interprocess/echo/EchoWorker.kt
+++ Echo/src/main/java/com/redelf/commons/interprocess/echo/EchoWorker.kt
@@ -5,8 +5,11 @@ import android.content.Intent
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.work.WorkerParameters
 import com.redelf.commons.interprocess.InterprocessWorker
+import com.redelf.commons.logging.Console
 
 class EchoWorker(ctx: Context, params: WorkerParameters) : InterprocessWorker(ctx, params) {
+
+    private val tag = "Echo ::"
 
     init {
 
@@ -16,9 +19,14 @@ class EchoWorker(ctx: Context, params: WorkerParameters) : InterprocessWorker(ct
     override fun onIntent(intent: Intent) {
 
         val message = intent.getStringExtra("data")
+
+        Console.log("Received echo request :: $message")
+
         val responseIntent = Intent(actions.first())
         responseIntent.putExtra("data", "Echo :: $message")
         LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(responseIntent)
+
+        Console.log("Sent echo response :: $message")
     }
 
     override fun doWork(): Result {
