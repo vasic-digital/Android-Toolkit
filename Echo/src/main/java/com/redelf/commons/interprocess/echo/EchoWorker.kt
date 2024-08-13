7642b4ca13d298b136ff760550d4fe7ed7781af8
Milos Vasic <i@mvasic.ru>
2024-08-13 20:38:34 +0300

    Working on interprocess worker.

--- Echo/src/main/java/com/redelf/commons/interprocess/echo/EchoWorker.kt
+++ Echo/src/main/java/com/redelf/commons/interprocess/echo/EchoWorker.kt
@@ -1,10 +1,16 @@
 package com.redelf.commons.interprocess.echo
 
 import android.content.Context
+import android.content.Intent
 import androidx.work.WorkerParameters
 import com.redelf.commons.interprocess.InterprocessWorker
 
 class EchoWorker(ctx: Context, params: WorkerParameters) : InterprocessWorker(ctx, params) {
+
+    override fun onIntent(intent: Intent) {
+
+        TODO("Not yet implemented")
+    }
 
     override fun doWork(): Result {
 
--- Interprocess/src/main/java/com/redelf/commons/interprocess/InterprocessWorker.kt
+++ Interprocess/src/main/java/com/redelf/commons/interprocess/InterprocessWorker.kt
@@ -1,7 +1,47 @@
 package com.redelf.commons.interprocess
 
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.redelf.commons.logging.Console
 
-abstract class InterprocessWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params)
+abstract class InterprocessWorker(ctx: Context, params: WorkerParameters) : Worker(ctx, params) {
+
+    private val broadcastReceiver = object : BroadcastReceiver() {
+
+        override fun onReceive(context: Context?, intent: Intent?) {
+
+            intent?.let {
+
+                onIntentReceived(it)
+            }
+        }
+    }
+
+    init {
+
+        actions().forEach {
+
+            LocalBroadcastManager.getInstance(ctx).registerReceiver(
+
+                broadcastReceiver,
+                IntentFilter(it)
+            )
+        }
+    }
+
+    private fun onIntentReceived(intent: Intent) {
+
+        Console.log("Received intent: ${intent.action}")
+
+        onIntent(intent)
+    }
+
+    protected abstract fun actions(): List<String>
+
+    protected abstract fun onIntent(intent: Intent)
+}
