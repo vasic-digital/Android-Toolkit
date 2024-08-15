619ee0247e3a3d7903bc7a620de2f5b0ae3d6992
Milos Vasic <i@mvasic.ru>
2024-08-15 19:09:54 +0300

    The Echo processor.

---
+++ Echo/src/main/java/com/redelf/commons/interprocess/echo/EchoInterprocessProcessor.kt
@@ -0,0 +1,48 @@
+package com.redelf.commons.interprocess.echo
+
+import android.content.Context
+import android.content.Intent
+import com.redelf.commons.interprocess.InterprocessProcessor
+import com.redelf.commons.logging.Console
+
+class EchoInterprocessProcessor(private val ctx: Context) : InterprocessProcessor {
+
+    companion object {
+
+        const val EXTRA_DATA = "data"
+        const val ACTION_ECHO = "com.redelf.commons.interprocess.echo"
+        const val ACTION_HELLO = "com.redelf.commons.interprocess.echo.hello"
+        const val ACTION_ECHO_RESPONSE = "com.redelf.commons.interprocess.echo.response"
+    }
+
+    private val echo = "Echo"
+    private val tag = "IPC service :: $echo ::"
+
+    override fun process(input: Intent) {
+
+        when (input.action) {
+
+            ACTION_HELLO -> hello()
+
+            ACTION_ECHO -> echo(input)
+        }
+    }
+
+    private fun hello() {
+
+        Console.log("$tag Hello from the Echo IPC")
+    }
+
+    private fun echo(intent: Intent) {
+
+        val message = intent.getStringExtra(EXTRA_DATA)
+
+        Console.log("$tag Request :: $message")
+
+        val responseIntent = Intent(ACTION_ECHO_RESPONSE)
+        responseIntent.putExtra(EXTRA_DATA, "$echo = $message")
+        ctx.applicationContext.sendBroadcast(responseIntent)
+
+        Console.log("$tag Response :: $message")
+    }
+}
--- Echo/src/main/java/com/redelf/commons/interprocess/echo/WelcomeActivity.kt
+++ Echo/src/main/java/com/redelf/commons/interprocess/echo/WelcomeActivity.kt
@@ -2,15 +2,13 @@ package com.redelf.commons.interprocess.echo
 
 import android.content.Intent
 import com.redelf.commons.activity.BaseActivity
-import com.redelf.commons.extensions.exec
-import org.junit.Assert
 
 class WelcomeActivity : BaseActivity() {
 
     override fun onPostResume() {
         super.onPostResume()
 
-        val intent = Intent(EchoService.ACTION_HELLO)
+        val intent = Intent(EchoInterprocessProcessor.ACTION_HELLO)
 
         applicationContext.sendBroadcast(intent)
     }
