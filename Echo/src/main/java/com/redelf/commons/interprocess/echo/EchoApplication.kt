413167e7d35ff73d4cf317a806ca6ce657d0fedc
Milos Vasic <i@mvasic.ru>
2024-08-15 19:17:43 +0300

    The Interprocess application.

--- Echo/src/main/java/com/redelf/commons/interprocess/echo/EchoApplication.kt
+++ Echo/src/main/java/com/redelf/commons/interprocess/echo/EchoApplication.kt
@@ -1,13 +1,25 @@
 package com.redelf.commons.interprocess.echo
 
+import android.content.ComponentName
 import android.content.Intent
+import android.content.ServiceConnection
+import android.os.IBinder
 import com.redelf.commons.application.BaseApplication
+import com.redelf.commons.interprocess.InterprocessApplication
+import com.redelf.commons.interprocess.InterprocessProcessor
 
-class EchoApplication : BaseApplication() {
+class EchoApplication : InterprocessApplication() {
 
     override val firebaseEnabled = false
 
     override fun isProduction() = false
 
     override fun takeSalt() = "echo_salt"
+
+    override fun getProcessors(): List<InterprocessProcessor> {
+
+        val echoProcessor = EchoInterprocessProcessor(applicationContext)
+
+        return listOf(echoProcessor)
+    }
 }
---
+++ Interprocess/src/main/java/com/redelf/commons/interprocess/InterprocessApplication.kt
@@ -0,0 +1,47 @@
+package com.redelf.commons.interprocess
+
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
+import android.os.IBinder
+import com.redelf.commons.application.BaseApplication
+import com.redelf.commons.logging.Console
+
+abstract class InterprocessApplication : BaseApplication() {
+
+    protected abstract fun getProcessors(): List<InterprocessProcessor>
+
+    private val connection = object : ServiceConnection {
+
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+
+            Console.log("Service connected")
+
+            service?.let {
+
+                if (it is InterprocessService.InterprocessBinder) {
+
+                    val processors = getProcessors()
+                    val interprocessService = it.getService()
+
+                    processors.forEach { processor -> interprocessService.register(processor) }
+                }
+            }
+
+            unbindService(this)
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+
+            Console.log("Service disconnected")
+        }
+    }
+
+    override fun onDoCreate() {
+        super.onDoCreate()
+
+        val serviceIntent = Intent(applicationContext, InterprocessService::class.java)
+        applicationContext.bindService(serviceIntent, connection, Context.BIND_AUTO_CREATE)
+    }
+}
