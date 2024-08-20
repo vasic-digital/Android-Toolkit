64d9aa79f4fa10e8733626d6dc63a54fd0b6445f
Milos Vasic <i@mvasic.ru>
2024-08-20 18:03:21 +0300

    Cleaning up.

--- Interprocess/src/main/java/com/redelf/commons/interprocess/InterprocessApplication.kt
+++ Interprocess/src/main/java/com/redelf/commons/interprocess/InterprocessApplication.kt
@@ -1,50 +1,14 @@
 package com.redelf.commons.interprocess
 
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
-import android.os.IBinder
 import com.redelf.commons.application.BaseApplication
-import com.redelf.commons.logging.Console
 
 abstract class InterprocessApplication : BaseApplication() {
 
     protected abstract fun getProcessors(): List<InterprocessProcessor>
 
-    private val connection = object : ServiceConnection {
-
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-
-            Console.log("Service connected")
-
-            service?.let {
-
-                if (it is Interprocessor.InterprocessBinder) {
-
-                    val processors = getProcessors()
-                    val interprocessService = it.getService()
-
-                    processors.forEach { processor -> interprocessService.register(processor) }
-                }
-            }
-
-            unbindService(this)
-        }
-
-        override fun onServiceDisconnected(name: ComponentName?) {
-
-            Console.log("Service disconnected")
-        }
-    }
-
     override fun onDoCreate() {
         super.onDoCreate()
 
-        val ctx = applicationContext
-        val serviceIntent = Intent(ctx, Interprocessor::class.java)
-
-        ctx.startService(serviceIntent)
-        ctx.bindService(serviceIntent, connection, Context.BIND_AUTO_CREATE)
+        getProcessors().forEach { processor -> Interprocessor.register(processor) }
     }
 }
