9172bb1abe7baadc80e5289f8dd5267716b57712
Milos Vasic <i@mvasic.ru>
2024-08-20 17:56:59 +0300

    The Interprocess(ing).

--- Interprocess/src/main/java/com/redelf/commons/interprocess/InterprocessReceiver.kt
+++ Interprocess/src/main/java/com/redelf/commons/interprocess/InterprocessReceiver.kt
@@ -1,14 +1,9 @@
 package com.redelf.commons.interprocess
 
 import android.content.BroadcastReceiver
-import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.content.ServiceConnection
-import android.os.IBinder
-import com.redelf.commons.application.BaseApplication
 import com.redelf.commons.logging.Console
-import java.util.concurrent.LinkedBlockingQueue
 
 class InterprocessReceiver : BroadcastReceiver() {
 
@@ -18,43 +13,10 @@ class InterprocessReceiver : BroadcastReceiver() {
     }
 
     private val tag = "IPC :: Receiver ::"
-    private val queue = LinkedBlockingQueue<Intent>()
 
     init {
 
         Console.log("$tag Created")
-    }
-
-    private val serviceConnection = object : ServiceConnection {
-
-        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
-
-            Console.log("$tag Service connected")
-
-            val context = BaseApplication.takeContext()
-
-            service?.let {
-
-                if (it is Interprocessor.InterprocessBinder) {
-
-                    val interprocessService = it.getService()
-                    val iterator = queue.iterator()
-
-                    while (iterator.hasNext()) {
-
-                        val intent = iterator.next()
-                        interprocessService.onIntent(intent)
-                    }
-                }
-            }
-
-            context.unbindService(this)
-        }
-
-        override fun onServiceDisconnected(name: ComponentName?) {
-
-            Console.log("$tag Service disconnected")
-        }
     }
 
     init {
@@ -64,9 +26,6 @@ class InterprocessReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
 
-        /*
-        * FIXME: Nothing is received here
-        */
         Console.log("$tag Received intent")
 
         intent?.let {
@@ -75,15 +34,7 @@ class InterprocessReceiver : BroadcastReceiver() {
 
                 Console.log("$tag Intent action :: ${it.action}")
 
-                context?.let { ctx ->
-
-                    queue.put(it)
-
-                    val serviceIntent = Intent(ctx, Interprocessor::class.java)
-
-                    ctx.startService(serviceIntent)
-                    ctx.bindService(serviceIntent, serviceConnection, Context.BIND_AUTO_CREATE)
-                }
+                Interprocessor.onIntent(it)
             }
         }
     }
