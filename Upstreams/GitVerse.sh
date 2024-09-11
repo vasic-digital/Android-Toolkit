0b13361b75e0ddf65153f69725f358db2ff5df3d
Milos Vasic <i@mvasic.ru>
2024-09-11 13:09:09 +0300

    URL fixed.

--- Upstreams/GitVerse.sh
+++ Upstreams/GitVerse.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-export UPSTREAMABLE_REPOSITORY="git@gitverse.ru:2222/redelf/Android_Toolkit.git"
+export UPSTREAMABLE_REPOSITORY="ssh://git@gitverse.ru:2222/redelf/Android_Toolkit.git"
