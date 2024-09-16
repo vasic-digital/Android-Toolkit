f2b170bffecb8219d922a65032788eab22be56c6
Milos Vasic <i@mvasic.ru>
2024-09-16 15:08:02 +0300

    Circular image.

--- Main/src/main/java/com/redelf/commons/extensions/views.kt
+++ Main/src/main/java/com/redelf/commons/extensions/views.kt
@@ -1,7 +1,12 @@
 package com.redelf.commons.extensions
 
 import android.text.Html
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions.bitmapTransform
+import com.redelf.commons.logging.Console
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation
 
 fun TextView.textAsHtml(htmlCode: String) {
 
@@ -11,4 +16,29 @@ fun TextView.textAsHtml(htmlCode: String) {
 fun TextView.textAsHtml(htmlCode: Int) {
 
     text = Html.fromHtml(context.getString(htmlCode), Html.FROM_HTML_MODE_COMPACT)
+}
+
+fun ImageView.circularImage(imgUrl: String, cornerRadius: Int) {
+
+    try {
+
+        Glide.with(this)
+            .load(imgUrl)
+            .apply(
+
+                bitmapTransform(
+
+                    RoundedCornersTransformation(
+
+                        cornerRadius, 0,
+                        RoundedCornersTransformation.CornerType.ALL
+                    )
+                )
+            )
+            .into(this)
+
+    } catch (e: Exception) {
+
+        Console.error(e)
+    }
 }
