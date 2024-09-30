48339a238622ae282635d012d49c702850adf0a6
Milos Vasic <i@mvasic.ru>
2024-09-30 17:50:59 +0300

    Auto-commit.

--- Analytics/src/main/java/com/redelf/analytics/implementation/facebook/FacebookAnalytics.kt
+++ Analytics/src/main/java/com/redelf/analytics/implementation/facebook/FacebookAnalytics.kt
@@ -1,9 +1,17 @@
 package com.redelf.analytics.implementation.facebook
 
+import android.os.Bundle
 import com.redelf.analytics.Analytics
 import com.redelf.analytics.AnalyticsParameter
 
+import com.facebook.appevents.AppEventsLogger
+import com.redelf.analytics.implementation.firebase.FirebaseAnalyticsEvent
+import com.redelf.commons.application.BaseApplication
+import com.redelf.commons.logging.Console
+
 class FacebookAnalytics : Analytics {
+
+    private val tag = "Analytics :: Facebook ::"
 
     override fun log(vararg params: AnalyticsParameter<*>) {
 
@@ -12,6 +20,26 @@ class FacebookAnalytics : Analytics {
             throw IllegalArgumentException("Firebase analytics parameters must be at least 3")
         }
 
-        TODO("Not yet implemented")
+        val bundle = Bundle()
+        val ctx = BaseApplication.takeContext()
+        val logger = AppEventsLogger.newLogger(ctx)
+
+        val name = params[0].obtain() as String
+        val key = params[1].obtain() as String
+        val value = params[2].obtain() as String
+
+        val analyticEvent = FirebaseAnalyticsEvent(name = name, param = Pair(key, value))
+
+        val paramLog = "Name = $name, Bundle :: Key: = '${analyticEvent.param?.first}', " +
+                "Value = '${analyticEvent.param?.second}'"
+
+        analyticEvent.param?.let {
+
+            bundle.putString(analyticEvent.param.first, analyticEvent.param.second)
+        }
+
+        logger.logEvent(name, bundle)
+
+        Console.log("$tag Logged event :: $paramLog")
     }
 }
