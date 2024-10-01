ab0235a63199fd1c1a1d1446bb5e023662bb69e5
Milos Vasic <i@mvasic.ru>
2024-10-01 12:09:54 +0300

    Small cleanup.

--- Main/src/main/java/com/redelf/commons/net/retrofit/RetrofitLogger.kt
+++ Main/src/main/java/com/redelf/commons/net/retrofit/RetrofitLogger.kt
@@ -4,6 +4,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import timber.log.Timber
 
 class RetrofitLogger : HttpLoggingInterceptor.Logger {
+
     override fun log(message: String) {
 
         Timber.v("Retrofit $message" )
