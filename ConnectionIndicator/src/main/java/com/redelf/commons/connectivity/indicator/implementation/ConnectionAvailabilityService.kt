c6e8ee004daf004432ebf9e6bdef21bd4ff5dbbc
Milos Vasic <i@mvasic.ru>
2024-10-18 11:52:59 +0300

    Connection available service.

---
+++ ConnectionIndicator/src/main/java/com/redelf/commons/connectivity/indicator/implementation/ConnectionAvailabilityService.kt
@@ -0,0 +1,8 @@
+package com.redelf.commons.connectivity.indicator.implementation
+
+import com.redelf.commons.connectivity.indicator.AvailableService
+
+class ConnectionAvailabilityService : AvailableService {
+
+
+}
