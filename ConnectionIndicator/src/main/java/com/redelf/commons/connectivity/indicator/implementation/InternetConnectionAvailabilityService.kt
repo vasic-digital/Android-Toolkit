abe28cb5a2d9d43c454b93593336887ac2596eec
Milos Vasic <i@mvasic.ru>
2024-10-20 18:12:52 +0300

    Implementation continued.

--- ConnectionIndicator/src/main/java/com/redelf/commons/connectivity/indicator/implementation/InternetConnectionAvailabilityService.kt
+++ ConnectionIndicator/src/main/java/com/redelf/commons/connectivity/indicator/implementation/InternetConnectionAvailabilityService.kt
@@ -4,6 +4,10 @@ import android.content.Context
 import com.redelf.commons.application.BaseApplication
 import com.redelf.commons.connectivity.indicator.connection.ConnectionAvailableService
 import com.redelf.commons.context.ContextAvailability
+import com.redelf.commons.logging.Console
+import com.redelf.commons.net.connectivity.ConnectionState
+import com.redelf.commons.net.connectivity.ConnectivityStateChanges
+import com.redelf.commons.net.connectivity.DefaultConnectivityHandler
 import com.redelf.commons.stateful.State
 
 class InternetConnectionAvailabilityService :
@@ -13,15 +17,49 @@ class InternetConnectionAvailabilityService :
 
 {
 
+    private val connectionHandler = DefaultConnectivityHandler(takeContext())
+
+    private val connectionCallback = object : ConnectivityStateChanges {
+
+        override fun onStateChanged() {
+
+            this@InternetConnectionAvailabilityService.onStateChanged()
+        }
+
+        override fun onState(state: State<Int>) {
+
+            Console.log("On state: $state")
+
+            this@InternetConnectionAvailabilityService.onState(state)
+        }
+
+        override fun getState(): State<Int> {
+
+            if (connectionHandler.isNetworkAvailable(takeContext())) {
+
+                return ConnectionState.Connected
+            }
+
+            return ConnectionState.Disconnected
+        }
+
+        override fun setState(state: State<Int>) {
+
+            this@InternetConnectionAvailabilityService.setState(state)
+        }
+    }
+
     override fun takeContext() = BaseApplication.takeContext()
 
     override fun onStateChanged() {
 
-        TODO("Not yet implemented")
+        // TODO: Implement onStateChanged()
     }
 
     override fun onState(state: State<Int>) {
 
-        TODO("Not yet implemented")
+        setState(state)
+
+        // TODO: Implement onState()
     }
 }
