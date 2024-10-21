a95df76446f064c501372411b1514639e58ed4c6
Milos Vasic <i@mvasic.ru>
2024-10-21 12:29:34 +0300

    Available service - First touches.

--- ConnectionIndicator/src/main/java/com/redelf/commons/connectivity/indicator/AvailableServices.kt
+++ ConnectionIndicator/src/main/java/com/redelf/commons/connectivity/indicator/AvailableServices.kt
@@ -1,13 +1,73 @@
 package com.redelf.commons.connectivity.indicator
 
-import java.util.PriorityQueue
+import com.redelf.commons.lifecycle.TerminationAsync
+import com.redelf.commons.lifecycle.TerminationSynchronized
+import com.redelf.commons.stateful.State
+import com.redelf.commons.stateful.Stateful
 
-abstract class AvailableServices(private val services: PriorityQueue<AvailableService>) {
+class AvailableServices(private val services: MutableSet<AvailableService>) :
 
+    AvailableService,
+    TerminationAsync,
+    Stateful<Any> // TODO: <---
 
+{
+
+    override fun onStateChanged() {
+
+        TODO("Not yet implemented")
+    }
+
+    override fun getState(): State<Any> {
+
+        TODO("Not yet implemented")
+    }
+
+    override fun setState(state: State<Any>) {
+
+        TODO("Not yet implemented")
+    }
+
+    override fun onState(state: State<Any>) {
+
+        TODO("Not yet implemented")
+    }
+
+    fun addService(service: AvailableService) {
+
+        services.add(service)
+    }
+
+    fun removeService(service: AvailableService) {
+
+        services.remove(service)
+    }
+
+    fun hasService(service: AvailableService): Boolean {
+
+        return services.contains(service)
+    }
 
     fun getServices(): List<AvailableService> {
 
         return services.toList()
+    }
+
+    override fun terminate() {
+
+        services.forEach {
+
+            if (it is TerminationAsync) {
+
+                it.terminate()
+            }
+
+            if (it is TerminationSynchronized) {
+
+                it.terminate()
+            }
+        }
+
+        services.clear()
     }
 }
