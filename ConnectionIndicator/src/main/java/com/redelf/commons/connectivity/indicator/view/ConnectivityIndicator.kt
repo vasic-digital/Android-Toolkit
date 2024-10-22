1d5330270d8766e3127b2a7b0d0b988db6cdd800
Milos Vasic <i@mvasic.ru>
2024-10-22 18:40:24 +0300

    View - First touches.

---
+++ ConnectionIndicator/src/main/java/com/redelf/commons/connectivity/indicator/view/ConnectivityIndicator.kt
@@ -0,0 +1,14 @@
+package com.redelf.commons.connectivity.indicator.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.RelativeLayout
+
+class ConnectivityIndicator(ctx: Context, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int)
+
+    : RelativeLayout(ctx, attrs, defStyleAttr, defStyleRes) {
+
+
+
+        // TODO: Implement the custom view here - #Availability
+}
