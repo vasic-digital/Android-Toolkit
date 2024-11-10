ed262be553b96cfcb91ef04dd46b96a72fc13381
Milos Vasic <i@mvasic.ru>
2024-11-10 14:59:36 +0300

    JSON extension.

---
+++ Main/src/main/java/com/redelf/commons/extensions/json.kt
@@ -0,0 +1,25 @@
+package com.redelf.commons.extensions
+
+import com.google.gson.Gson
+import com.redelf.commons.logging.Console
+
+private val gson = Gson()
+
+fun json(what: Any): String {
+
+    try {
+
+        return gson.toJson(what)
+
+    } catch (e: Exception) {
+
+        Console.error(e)
+    }
+
+    return ""
+}
+
+fun Any.json(): String {
+
+    return json(this)
+}
