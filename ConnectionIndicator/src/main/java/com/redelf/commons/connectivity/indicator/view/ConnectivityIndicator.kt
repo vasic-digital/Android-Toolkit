60dfb91956db107262b3a52a4493a7d650bf360f
Milos Vasic <i@mvasic.ru>
2024-11-12 14:23:48 +0300

    Test color parameter set.

--- ConnectionIndicator/src/main/java/com/redelf/commons/connectivity/indicator/view/ConnectivityIndicator.kt
+++ ConnectionIndicator/src/main/java/com/redelf/commons/connectivity/indicator/view/ConnectivityIndicator.kt
@@ -38,6 +38,7 @@ class ConnectivityIndicator :
     var origin = ""
     var dialogStyle = 0
     var showDetails = false
+    var textColor: Int? = null
     var dialogLayout = R.layout.dialog_services_states
     var dialogAdapterItemLayout: Int = R.layout.layout_services_states_dialog_adapter
 
@@ -360,6 +361,7 @@ class ConnectivityIndicator :
                     ctx,
                     dialogStyle,
                     dialogLayout,
+                    textColor,
                     dialogAdapterItemLayout,
                     builder = it,
                     serviceCallback = serviceCallback
