f35ae0564580647bf1ccc0ed006f8e2ed2e15cf1
Milos Vasic <i@mvasic.ru>
2024-12-01 17:05:33 +0300

    FIXME added.

--- Main/src/main/java/com/redelf/commons/management/LazyDataManagement.kt
+++ Main/src/main/java/com/redelf/commons/management/LazyDataManagement.kt
@@ -237,6 +237,9 @@ abstract class LazyDataManagement<T> : DataManagement<T>(), Registration<Context
         if (DEBUG.get()) Console.log("Application is in foreground")
     }
 
+    /*
+    * FIXME: The method is fired even we are surfing through the screens
+    */
     private fun onBackground(from: String) {
 
         val tag = "Lazy :: Who = '${getWho()}', From = '$from' :: BACKGROUND ::"
