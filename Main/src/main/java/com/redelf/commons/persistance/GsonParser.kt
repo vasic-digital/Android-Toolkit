dcbc58dd3cd9f9abafa21f2d73bbb843f92e68c8
Milos Vasic <i@mvasic.ru>
2024-12-02 16:26:41 +0300

    Apply customizations.

--- Main/src/main/java/com/redelf/commons/persistance/GsonParser.kt
+++ Main/src/main/java/com/redelf/commons/persistance/GsonParser.kt
@@ -6,9 +6,18 @@ import com.redelf.commons.extensions.recordException
 import com.redelf.commons.logging.Console
 import com.redelf.commons.obtain.Obtain
 import com.redelf.commons.persistance.base.Parser
+import com.redelf.commons.persistance.serialization.CustomSerializable
 import java.lang.reflect.Type
+import java.util.concurrent.atomic.AtomicBoolean
 
 class GsonParser(private val provider: Obtain<Gson>) : Parser {
+
+    companion object {
+
+        val DEBUG = AtomicBoolean()
+    }
+
+    private val tag = "Parser :: GSON ::"
 
     override fun <T> fromJson(content: String?, type: Type?): T? {
 
@@ -54,9 +63,35 @@ class GsonParser(private val provider: Obtain<Gson>) : Parser {
 
     override fun toJson(body: Any?): String? {
 
+        if (body == null) {
+
+            return null
+        }
+
+        if (DEBUG.get()) Console.log("$tag START :: Class = ${body::class.java.canonicalName}")
+
         try {
 
-            return provider.obtain().toJson(body)
+            val gsonProvider = provider.obtain()
+
+            if (body is CustomSerializable) {
+
+                val customizations = body.getCustomSerializations()
+
+                if (DEBUG.get()) {
+
+                    Console.log(
+
+                        "$tag Custom serialization :: " +
+                                "Class = ${body::class.java.canonicalName}, " +
+                                "Customizations = $customizations"
+                    )
+                }
+
+                // TODO: Apply customizations
+            }
+
+            return gsonProvider.toJson(body)
 
         } catch (e: Exception) {
 
