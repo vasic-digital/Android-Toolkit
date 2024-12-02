0fc6205a370e844a8b81cde858e5a2feefe64ccc
Milos Vasic <i@mvasic.ru>
2024-12-02 18:34:26 +0300

    Implementing the dynamic type adapter.

--- Main/src/main/java/com/redelf/commons/persistance/serialization/CustomSerializable.kt
+++ Main/src/main/java/com/redelf/commons/persistance/serialization/CustomSerializable.kt
@@ -2,5 +2,11 @@ package com.redelf.commons.persistance.serialization
 
 interface CustomSerializable {
 
+    /*
+    * TODO:
+    *  - Make sure that we can provide recipe for the whole type
+    *  - Vs. just the fields
+    */
+
     fun getCustomSerializations(): Map<String, String>
 }
