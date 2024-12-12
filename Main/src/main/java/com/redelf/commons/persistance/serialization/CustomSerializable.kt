8b1aa8188484d665809c64ebbc4f7ae383092587
Milos Vasic <i@mvasic.ru>
2024-12-12 20:30:28 +0300

    TODOs added.

--- Main/src/main/java/com/redelf/commons/persistance/serialization/CustomSerializable.kt
+++ Main/src/main/java/com/redelf/commons/persistance/serialization/CustomSerializable.kt
@@ -4,6 +4,8 @@ interface CustomSerializable {
 
     /*
     * TODO:
+    *  - Make annotation version of this
+    *  - Support getCustomSerializations vi annotations too
     *  - Make sure that we can provide recipe for the whole type
     *  - Vs. just the fields
     */
