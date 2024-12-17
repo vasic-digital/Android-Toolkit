afaeeebcae1dda204c465b81e502e96eafe80245
Milos Vasic <i@mvasic.ru>
2024-12-17 12:41:13 +0300

    Default constructor added.

--- Main/src/main/java/com/redelf/commons/data/type/PairDataInfo.kt
+++ Main/src/main/java/com/redelf/commons/data/type/PairDataInfo.kt
@@ -2,8 +2,12 @@ package com.redelf.commons.data.type
 
 data class PairDataInfo(
 
-    var first: Any,
-    var second: Any,
-    var firstType: String? = first::class.java.canonicalName,
-    var secondType: String? = first::class.java.canonicalName
-)
+    var first: Any? = null,
+    var second: Any? = null,
+    var firstType: String? = first?.javaClass?.canonicalName,
+    var secondType: String? = second?.javaClass?.canonicalName
+
+) {
+
+    constructor() : this(null, null, null, null)
+}
