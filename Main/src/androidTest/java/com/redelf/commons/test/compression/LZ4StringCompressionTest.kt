65af15da060acdd001ff9d498bc2044efd8eab17
Milos Vasic <i@mvasic.ru>
2025-01-17 20:04:58 +0300

    FIXME added.

--- Main/src/androidTest/java/com/redelf/commons/test/compression/LZ4StringCompressionTest.kt
+++ Main/src/androidTest/java/com/redelf/commons/test/compression/LZ4StringCompressionTest.kt
@@ -51,38 +51,91 @@ class LZ4StringCompressionTest : BaseTest() {
 
         val tag = "Test :: LZ4WithEncryption ::"
 
-        val text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, " +
+        var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, " +
                 "nulla sit amet ultrices ultrices, ante massa tincidunt ante, eu " +
                 "tincidunt turpis ante eu ante."
 
         Console.log("$tag START")
 
-        var start = System.currentTimeMillis()
+        /*
+        * FIXME: java.lang.OutOfMemoryError: Failed to allocate a 311951376 byte allocation with 25165824 free bytes and 191MB until OOM, target footprint 361643744, growth limit 536870912
+            at java.util.Arrays.copyOf(Arrays.java:3585)
+            at java.io.ByteArrayOutputStream.grow(ByteArrayOutputStream.java:120)
+            at java.io.ByteArrayOutputStream.ensureCapacity(ByteArrayOutputStream.java:95)
+            at java.io.ByteArrayOutputStream.write(ByteArrayOutputStream.java:156)
+            at com.redelf.commons.extensions.StringsKt.decompress(strings.kt:156)
+            at com.redelf.commons.extensions.StringsKt.decryptAndDecompress(strings.kt:230)
+            at com.redelf.commons.extensions.StringsKt.decryptAndDecompress$default(strings.kt:213)
+            at com.redelf.commons.test.compression.LZ4StringCompressionTest.testLZ4WithEncryption(LZ4StringCompressionTest.kt:82)
+            at java.lang.reflect.Method.invoke(Native Method)
+            at org.junit.runners.model.FrameworkMethod$1.runReflectiveCall(FrameworkMethod.java:59)
+            at org.junit.internal.runners.model.ReflectiveCallable.run(ReflectiveCallable.java:12)
+            at org.junit.runners.model.FrameworkMethod.invokeExplosively(FrameworkMethod.java:56)
+            at org.junit.internal.runners.statements.InvokeMethod.evaluate(InvokeMethod.java:17)
+            at androidx.test.internal.runner.junit4.statement.RunBefores.evaluate(RunBefores.java:80)
+            at org.junit.runners.ParentRunner$3.evaluate(ParentRunner.java:306)
+            at org.junit.runners.BlockJUnit4ClassRunner$1.evaluate(BlockJUnit4ClassRunner.java:100)
+            at org.junit.runners.ParentRunner.runLeaf(ParentRunner.java:366)
+            at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:103)
+            at org.junit.runners.BlockJUnit4ClassRunner.runChild(BlockJUnit4ClassRunner.java:63)
+            at org.junit.runners.ParentRunner$4.run(ParentRunner.java:331)
+            at org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:79)
+            at org.junit.runners.ParentRunner.runChildren(ParentRunner.java:329)
+            at org.junit.runners.ParentRunner.access$100(ParentRunner.java:66)
+            at org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:293)
+            at org.junit.runners.ParentRunner$3.evaluate(ParentRunner.java:306)
+            at org.junit.runners.ParentRunner.run(ParentRunner.java:413)
+            at org.junit.runners.Suite.runChild(Suite.java:128)
+            at org.junit.runners.Suite.runChild(Suite.java:27)
+            at org.junit.runners.ParentRunner$4.run(ParentRunner.java:331)
+            at org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:79)
+            at org.junit.runners.ParentRunner.runChildren(ParentRunner.java:329)
+            at org.junit.runners.ParentRunner.access$100(ParentRunner.java:66)
+            at org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:293)
+            at org.junit.runners.ParentRunner$3.evaluate(ParentRunner.java:306)
+            at org.junit.runners.ParentRunner.run(ParentRunner.java:413)
+            at org.junit.runner.JUnitCore.run(JUnitCore.java:137)
+            at org.junit.runner.JUnitCore.run(JUnitCore.java:115)
+            at androidx.test.internal.runner.TestExecutor.execute(TestExecutor.java:68)
+            at androidx.test.internal.runner.TestExecutor.execute(TestExecutor.java:59)
+            at androidx.test.runner.AndroidJUnitRunner.onStart(AndroidJUnitRunner.java:463)
+            at android.app.Instrumentation$InstrumentationThread.run(Instrumentation.java:2415)
+        */
+        (0..100).forEach {
 
-        val compressed = text.compressAndEncrypt()
+            text += text
 
-        Console.log(
+            val tag = "$tag Iteration = ${it + 1} ::"
 
-            "$tag COMPRESSED :: Time = ${System.currentTimeMillis() - start} ms, " +
-                    "Size = ${compressed.toByteArray().size} bytes, " +
-                    "Original Size = ${text.toByteArray().size} bytes"
-        )
+            var start = System.currentTimeMillis()
 
-        Assert.assertNotNull(compressed)
-        Assert.assertNotEquals(text, compressed)
+            val compressed = text.compressAndEncrypt()
 
-        start = System.currentTimeMillis()
+            Console.log(
 
-        val decompressed = compressed.decryptAndDecompress()
+                "$tag COMPRESSED :: Time = ${System.currentTimeMillis() - start} ms, " +
+                        "Size = ${compressed.toByteArray().size} bytes, " +
+                        "Original Size = ${text.toByteArray().size} bytes"
+            )
 
-        Console.log(
+            Assert.assertNotNull(compressed)
+            Assert.assertNotEquals(text, compressed)
 
-            "$tag DECOMPRESSED :: Time = ${System.currentTimeMillis() - start} ms, " +
-                    "Size = ${decompressed.toByteArray().size} bytes, " +
-                    "Original Size = ${text.toByteArray().size} bytes"
-        )
+            start = System.currentTimeMillis()
 
-        Assert.assertEquals(text, decompressed)
-        Assert.assertTrue(compressed.isNotEmpty())
+            val decompressed = compressed.decryptAndDecompress()
+
+            Console.log(
+
+                "$tag DECOMPRESSED :: Time = ${System.currentTimeMillis() - start} ms, " +
+                        "Size = ${decompressed.toByteArray().size} bytes, " +
+                        "Original Size = ${text.toByteArray().size} bytes"
+            )
+
+            Assert.assertEquals(text, decompressed)
+            Assert.assertTrue(compressed.isNotEmpty())
+        }
+
+        Console.log("$tag END")
     }
 }
