27ffdf938f9bdd1278f65490e8c2419939cf2741
Milos Vasic <i@mvasic.ru>
2025-02-12 13:10:15 +0300

    Fit inside system boundaries.

--- Main/src/main/java/com/redelf/commons/extensions/activity.kt
+++ Main/src/main/java/com/redelf/commons/extensions/activity.kt
@@ -3,12 +3,26 @@ package com.redelf.commons.extensions
 import android.app.Activity
 import android.os.Build
 import android.view.WindowInsets
+import androidx.fragment.app.DialogFragment
 
 fun Activity.fitInsideSystemBoundaries() {
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
 
         window.decorView.setOnApplyWindowInsetsListener { view, insets ->
+
+            val systemBarsInsets = insets.getInsets(WindowInsets.Type.systemBars())
+            view.setPadding(0, systemBarsInsets.top, 0, systemBarsInsets.bottom)
+            insets
+        }
+    }
+}
+
+fun DialogFragment.fitInsideSystemBoundaries() {
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+
+        activity?.window?.decorView?.setOnApplyWindowInsetsListener { view, insets ->
 
             val systemBarsInsets = insets.getInsets(WindowInsets.Type.systemBars())
             view.setPadding(0, systemBarsInsets.top, 0, systemBarsInsets.bottom)
