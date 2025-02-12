c9f7d9fd9f97afac6c4ce93a7db813b675ef1414
Milos Vasic <i@mvasic.ru>
2025-02-12 13:48:03 +0300

    Fit inside system boundaries.

--- Main/src/main/java/com/redelf/commons/activity/BaseDialogFragment.kt
+++ Main/src/main/java/com/redelf/commons/activity/BaseDialogFragment.kt
@@ -1,13 +1,30 @@
 package com.redelf.commons.activity
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.redelf.commons.extensions.fitInsideSystemBoundaries
 
 abstract class BaseDialogFragment : DialogFragment() {
 
-    override fun onStart() {
-        super.onStart()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
         fitInsideSystemBoundaries()
+    }
+
+    override fun onCreateView(
+
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+
+    ): View? {
+
+        fitInsideSystemBoundaries()
+
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 }
