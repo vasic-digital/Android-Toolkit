69d927d2d97da9c229e93bbad009bc197843a329
Milos Vasic <i@mvasic.ru>
2025-02-12 14:15:14 +0300

    Fit inside system boundaries.

--- Main/src/main/java/com/redelf/commons/activity/BaseDialogFragment.kt
+++ Main/src/main/java/com/redelf/commons/activity/BaseDialogFragment.kt
@@ -1,30 +1,13 @@
 package com.redelf.commons.activity
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.redelf.commons.extensions.fitInsideSystemBoundaries
 
 abstract class BaseDialogFragment : DialogFragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onResume() {
+        super.onResume()
 
         fitInsideSystemBoundaries()
-    }
-
-    override fun onCreateView(
-
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-
-    ): View? {
-
-        fitInsideSystemBoundaries()
-
-        return super.onCreateView(inflater, container, savedInstanceState)
     }
 }
--- Main/src/main/java/com/redelf/commons/extensions/activity.kt
+++ Main/src/main/java/com/redelf/commons/extensions/activity.kt
@@ -2,7 +2,10 @@ package com.redelf.commons.extensions
 
 import android.app.Activity
 import android.os.Build
+import android.view.View
 import android.view.WindowInsets
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.DialogFragment
 
 fun Activity.fitInsideSystemBoundaries() {
@@ -18,15 +21,30 @@ fun Activity.fitInsideSystemBoundaries() {
     }
 }
 
+fun Activity.getSystemBarsInsets(onInsetsChanged: (top: Int, bottom: Int) -> Unit) {
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+
+        val rootView: View = window.decorView.findViewById(android.R.id.content)
+
+        ViewCompat.setOnApplyWindowInsetsListener(rootView) { _, insets ->
+
+            val systemBarsInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            onInsetsChanged(systemBarsInsets.top, systemBarsInsets.bottom)
+            insets
+        }
+
+        rootView.requestApplyInsets()
+    }
+}
+
 fun DialogFragment.fitInsideSystemBoundaries() {
 
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
 
-        activity?.window?.decorView?.setOnApplyWindowInsetsListener { view, insets ->
+        activity?.getSystemBarsInsets { top, bottom ->
 
-            val systemBarsInsets = insets.getInsets(WindowInsets.Type.systemBars())
-            view.setPadding(0, systemBarsInsets.top, 0, systemBarsInsets.bottom)
-            insets
+            view?.setPadding(0, top, 0, bottom)
         }
     }
 }
