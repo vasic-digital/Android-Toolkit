284a567cf386ea61fa92e011606acc9098cabcd3
Milos Vasic <i@mvasic.ru>
2025-05-24 11:48:06 +0300

    Auto-commit

---
+++ Main/src/main/java/com/redelf/commons/retrofit/extensions.kt
@@ -0,0 +1,29 @@
+package com.redelf.commons.retrofit
+
+import com.redelf.commons.logging.Console
+import retrofit2.Response
+
+fun <T> Response<T>.close() {
+
+    val tag = "Response :: $this ::"
+
+    try {
+
+        val raw = this.raw()
+
+        Console.log("$tag Closing")
+
+        this.close()
+
+        Console.log("$tag Closed")
+
+        if (raw == null) {
+
+            Console.warning("$tag No raw response to close")
+        }
+
+    } catch (_: Exception) {
+
+        // Ignore
+    }
+}
