e6946bd62bfb2f9ba7de08bca1946e70ffb01961
Milos Vasic <i@mvasic.ru>
2025-06-06 13:20:55 +0300

    Callbacks.

--- Main/src/main/java/com/redelf/commons/deep_linking/DeepLinkController.kt
+++ Main/src/main/java/com/redelf/commons/deep_linking/DeepLinkController.kt
@@ -2,5 +2,5 @@ package com.redelf.commons.deep_linking
 
 interface DeepLinkController {
 
-    fun handleDeepLink(parameter: String, callback: Runnable)
+    fun handleDeepLink(parameter: String, onSuccess: () -> Unit, onError: () -> Unit)
 }
