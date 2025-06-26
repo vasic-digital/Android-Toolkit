937699987a6e8301e63abb72a813bc2b909c0f46
Milos Vasic <i@mvasic.ru>
2025-06-26 20:51:26 +0300

    Context extensions.

---
+++ Main/src/main/java/com/redelf/commons/extensions/context.kt
@@ -0,0 +1,68 @@
+package com.redelf.commons.extensions
+
+import android.app.Activity
+import android.app.ActivityOptions
+import android.content.Context
+import android.content.Intent
+import android.hardware.display.DisplayManager
+import android.view.Display
+import com.redelf.commons.logging.Console
+
+const val EXTRA_DISPLAY_ID = "Extra.Display.ID"
+
+fun Context.startActivityOnExtendedDisplay(what: Class<*>, from: String? = this::class.simpleName) {
+
+    var displayId = 10
+    var targetDisplay: Display? = null
+    val tag = "ATMOSphere :: Launching on external display :: From = '$from' ::"
+    val displayManager = getSystemService(Context.DISPLAY_SERVICE) as DisplayManager
+    val displays = displayManager.displays
+
+    Console.debug("$tag Looking for the display")
+
+    while (displayId > 0 && targetDisplay == null) {
+
+        targetDisplay = displays.firstOrNull { it.displayId == displayId }
+
+        if (targetDisplay == null) {
+
+            displayId--
+        }
+    }
+
+    targetDisplay?.let {
+
+        val intent = Intent(this, what)
+        intent.putExtra(EXTRA_DISPLAY_ID, displayId)
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+        val options = ActivityOptions.makeBasic().setLaunchDisplayId(displayId)
+
+        try {
+
+            startActivity(intent, options.toBundle())
+
+            Console.info("$tag Display found :: ID = $displayId")
+
+        } catch (e: Throwable) {
+
+            recordException(e)
+        }
+
+        if (this is Activity && !isFinishing) {
+
+            finish()
+        }
+    }
+
+    if (targetDisplay == null) {
+
+        Console.error("$tag No target display to present")
+
+        if (this is Activity && !isFinishing) {
+
+            Console.log("$tag Finishing")
+
+            finish()
+        }
+    }
+}
