d6b05945ab2e066d4d1173686fb9768d0bbe3a6a
Milos Vasic <i@mvasic.ru>
2025-07-10 11:58:57 +0300

    In and out animation resources props.

--- Main/src/main/java/com/redelf/commons/activity/TransitionEffectsActivity.kt
+++ Main/src/main/java/com/redelf/commons/activity/TransitionEffectsActivity.kt
@@ -1,4 +1,7 @@
 package com.redelf.commons.activity
 
 abstract class TransitionEffectsActivity : BaseActivity() {
+
+    protected open val inEffect: Int = 0
+    protected open val outEffect: Int = 0
 }
