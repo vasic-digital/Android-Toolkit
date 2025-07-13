c2495b4fbf544cd090f0326e0ab4ae6e38fe8a59
Milos Vasic <i@mvasic.ru>
2025-07-13 13:57:22 +0300

    List wrapper test - First touches.

---
+++ Main/src/androidTest/java/com/redelf/commons/test/ListWrapperTest.kt
@@ -0,0 +1,38 @@
+package com.redelf.commons.test
+
+class ListWrapperTest : BaseTest() {
+
+    /*
+
+        The test asserts the following methods:
+
+        - add
+        - get
+        - remove index
+        - remove item
+        - update
+        - indexOf
+        - removeAll
+        - clear
+        - replaceAllAndFilter
+        - addAllAndFilter
+        - addAll
+        - purge
+        - getSize
+    */
+
+    fun testAdd() {}
+    fun testGet() {}
+    fun testRemoveIndex() {}
+    fun testRemoveItem() {}
+    fun testUpdate() {}
+    fun testIndexOf() {}
+    fun testRemoveAll() {}
+    fun testClear() {}
+    fun testReplaceAndFilter() {}
+    fun testAddAllAndFilter() {}
+    fun testAddAll() {}
+    fun testPurge() {}
+    fun testGetSize() {}
+
+}
