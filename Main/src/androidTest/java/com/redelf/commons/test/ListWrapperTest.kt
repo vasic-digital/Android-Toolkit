fd0430f4b084e6dea8fc3faab31ff4a0a5dd1b51
Milos Vasic <i@mvasic.ru>
2025-07-13 15:22:59 +0300

    List wrapper test - WIP.

--- Main/src/androidTest/java/com/redelf/commons/test/ListWrapperTest.kt
+++ Main/src/androidTest/java/com/redelf/commons/test/ListWrapperTest.kt
@@ -1,5 +1,6 @@
 package com.redelf.commons.test
 
+import com.redelf.commons.data.model.Wrapper
 import com.redelf.commons.data.wrappers.ListWrapper
 import org.junit.Assert
 import org.junit.Test
@@ -68,9 +69,9 @@ class ListWrapperTest : BaseTest() {
             Assert.assertTrue(wrapper.getList() == collection)
             Assert.assertTrue(wrapper.getSize() == collection.size)
 
-            Assert.assertEquals(1, wrapper.get(0))
-            Assert.assertEquals(3, wrapper.get(1))
-            Assert.assertEquals(5, wrapper.get(2))
+            Assert.assertEquals(1, wrapper.get(0)?.takeData())
+            Assert.assertEquals(3, wrapper.get(1)?.takeData())
+            Assert.assertEquals(5, wrapper.get(2)?.takeData())
         }
     }
 
@@ -80,18 +81,16 @@ class ListWrapperTest : BaseTest() {
         listOf(true, false).forEachIndexed { index, onUI ->
 
             val collection = createCollection()
-            val challengeData = createChallengeCollection()
             val wrapper = createWrapper(collection)
 
             Assert.assertTrue(collection.isNotEmpty())
-            Assert.assertTrue(challengeData.isNotEmpty())
-            Assert.assertTrue(collection.size == challengeData.size)
             Assert.assertTrue(wrapper.getList() == collection)
             Assert.assertTrue(wrapper.getSize() == collection.size)
 
+            wrapper.remove("test", index = 0)
+
+            Assert.assertEquals(3, wrapper.get(0)?.takeData())
         }
-
-
     }
 
     @Test
@@ -100,18 +99,18 @@ class ListWrapperTest : BaseTest() {
         listOf(true, false).forEachIndexed { index, onUI ->
 
             val collection = createCollection()
-            val challengeData = createChallengeCollection()
             val wrapper = createWrapper(collection)
+            val what = collection[1]
+            val initSize = collection.size
 
-            Assert.assertTrue(collection.isNotEmpty())
-            Assert.assertTrue(challengeData.isNotEmpty())
-            Assert.assertTrue(collection.size == challengeData.size)
+            Assert.assertTrue(true)
             Assert.assertTrue(wrapper.getList() == collection)
             Assert.assertTrue(wrapper.getSize() == collection.size)
 
+            wrapper.remove("test", what = what)
+
+            Assert.assertEquals(initSize - 1, wrapper.getSize())
         }
-
-
     }
 
     @Test
@@ -130,8 +129,6 @@ class ListWrapperTest : BaseTest() {
             Assert.assertTrue(wrapper.getSize() == collection.size)
 
         }
-
-
     }
 
     @Test
@@ -249,7 +246,7 @@ class ListWrapperTest : BaseTest() {
             Assert.assertTrue(wrapper.getList() == collection)
             Assert.assertTrue(wrapper.getSize() == collection.size)
 
-            wrapper.addAll( challengeData, "testAdd.all")
+            wrapper.addAll(challengeData, "testAdd.all")
 
             val size = wrapper.getSize()
             val defaultSize = createCollection().size
@@ -305,16 +302,12 @@ class ListWrapperTest : BaseTest() {
 
     }
 
-    private fun createCollection() = mutableListOf(1, 3, 5, 7, 9)
+    private fun createCollection() =
+        mutableListOf(Wrapper(1), Wrapper(3), Wrapper(5), Wrapper(7), Wrapper(9))
 
-    private fun createChallengeCollection() = mutableListOf(2, 4, 6, 8, 10)
+    private fun createChallengeCollection() =
+        mutableListOf(Wrapper(2), Wrapper(4), Wrapper(6), Wrapper(8), Wrapper(10))
 
-    private fun createWrapper(collection: MutableList<Int>, onUI: Boolean = true) =
+    private fun createWrapper(collection: MutableList<Wrapper<Int>>, onUI: Boolean = true) =
         ListWrapper("test", "test.ui=$onUI", onUI, collection)
-
-    private fun createWrappers(collection: MutableList<Int>) = listOf(
-
-        createWrapper(collection, true ),
-        createWrapper(collection, false )
-    )
 }
