7d66161c9fc0275204382f43cabe1e9be549d903
Milos Vasic <i@mvasic.ru>
2025-07-13 14:07:40 +0300

    List wrapper test - WIP.

--- Main/src/androidTest/java/com/redelf/commons/test/ListWrapperTest.kt
+++ Main/src/androidTest/java/com/redelf/commons/test/ListWrapperTest.kt
@@ -1,5 +1,7 @@
 package com.redelf.commons.test
 
+import com.redelf.commons.data.wrappers.ListWrapper
+import org.junit.Assert
 import org.junit.Test
 
 class ListWrapperTest : BaseTest() {
@@ -24,41 +26,170 @@ class ListWrapperTest : BaseTest() {
     */
 
     @Test
-    fun testAdd() {}
+    fun testAdd() {
+
+        val testData = instantiateTestData()
+        val collection = testData[0]
+        val challengeData = testData[1]
+        val wrapper = testData[2]
+
+
+    }
 
     @Test
-    fun testGet() {}
+    fun testGet() {
+
+        val testData = instantiateTestData()
+        val collection = testData[0]
+        val challengeData = testData[1]
+        val wrapper = testData[2]
+
+
+    }
 
     @Test
-    fun testRemoveIndex() {}
+    fun testRemoveIndex() {
+
+        val testData = instantiateTestData()
+        val collection = testData[0]
+        val challengeData = testData[1]
+        val wrapper = testData[2]
+
+
+    }
 
     @Test
-    fun testRemoveItem() {}
+    fun testRemoveItem() {
+
+        val testData = instantiateTestData()
+        val collection = testData[0]
+        val challengeData = testData[1]
+        val wrapper = testData[2]
+
+
+    }
 
     @Test
-    fun testUpdate() {}
+    fun testUpdate() {
+
+        val testData = instantiateTestData()
+        val collection = testData[0]
+        val challengeData = testData[1]
+        val wrapper = testData[2]
+
+
+    }
 
     @Test
-    fun testIndexOf() {}
+    fun testIndexOf() {
+
+        val testData = instantiateTestData()
+        val collection = testData[0]
+        val challengeData = testData[1]
+        val wrapper = testData[2]
+
+
+    }
 
     @Test
-    fun testRemoveAll() {}
+    fun testRemoveAll() {
+
+        val testData = instantiateTestData()
+        val collection = testData[0]
+        val challengeData = testData[1]
+        val wrapper = testData[2]
+
+
+    }
 
     @Test
-    fun testClear() {}
+    fun testClear() {
+
+        val testData = instantiateTestData()
+        val collection = testData[0]
+        val challengeData = testData[1]
+        val wrapper = testData[2]
+
+
+    }
 
     @Test
-    fun testReplaceAndFilter() {}
+    fun testReplaceAndFilter() {
+
+        val testData = instantiateTestData()
+        val collection = testData[0]
+        val challengeData = testData[1]
+        val wrapper = testData[2]
+
+
+    }
 
     @Test
-    fun testAddAllAndFilter() {}
+    fun testAddAllAndFilter() {
+
+        val testData = instantiateTestData()
+        val collection = testData[0]
+        val challengeData = testData[1]
+        val wrapper = testData[2]
+
+
+    }
 
     @Test
-    fun testAddAll() {}
+    fun testAddAll() {
+
+        val testData = instantiateTestData()
+        val collection = testData[0]
+        val challengeData = testData[1]
+        val wrapper = testData[2]
+
+
+    }
 
     @Test
-    fun testPurge() {}
+    fun testPurge() {
+
+        val testData = instantiateTestData()
+        val collection = testData[0]
+        val challengeData = testData[1]
+        val wrapper = testData[2]
+
+
+    }
 
     @Test
-    fun testGetSize() {}
+    fun testGetSize() {
+
+        val testData = instantiateTestData()
+        val collection = testData[0]
+        val challengeData = testData[1]
+        val wrapper = testData[2]
+
+
+    }
+
+    private fun createCollection() = mutableListOf(1, 3, 5, 7, 9)
+
+    private fun createChallengeCollection() = mutableListOf(2, 4, 6, 8, 10)
+
+    private fun createWrapper(collection: MutableList<Int>) =
+        ListWrapper("test", "test", true, collection)
+
+    private fun instantiateTestData(): List<Any> {
+
+        val collection = createCollection()
+        val challengeData = createChallengeCollection()
+        val wrapper = createWrapper(collection)
+
+        Assert.assertTrue(collection.isNotEmpty())
+        Assert.assertTrue(challengeData.isNotEmpty())
+        Assert.assertTrue(collection.size == challengeData.size)
+        Assert.assertTrue(wrapper.getList() == collection)
+        Assert.assertTrue(wrapper.getSize() == collection.size)
+
+        return listOf(
+
+            collection, challengeData, wrapper
+        )
+    }
 }
