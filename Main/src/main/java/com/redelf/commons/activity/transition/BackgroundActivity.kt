d758ed267179f84a92b1fb9b36a7ee8e41175785
Milos Vasic <i@mvasic.ru>
2025-07-16 14:05:49 +0300

    Wrapping into the groups - Work in progress.

--- Main/src/main/java/com/redelf/commons/activity/transition/BackgroundActivity.kt
+++ Main/src/main/java/com/redelf/commons/activity/transition/BackgroundActivity.kt
@@ -3,12 +3,35 @@ package com.redelf.commons.activity.transition
 import android.os.Bundle
 import com.redelf.commons.R
 import com.redelf.commons.activity.base.BaseActivity
+import com.redelf.commons.logging.Console
 
 class BackgroundActivity : BaseActivity() {
+
+    private val tag = "Backgroud activity ::"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_background)
+
+        Console.log("$tag onCreate")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Console.log("$tag onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Console.log("$tag onPause")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Console.log("$tag onDestroy")
     }
 }
