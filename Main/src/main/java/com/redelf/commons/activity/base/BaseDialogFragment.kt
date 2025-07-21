2540f297dfbdc02f877f7a50fe8300e003faee1c
Milos Vasic <i@mvasic.ru>
2025-07-21 13:20:30 +0300

    BAck press nadling.

--- Main/src/main/java/com/redelf/commons/activity/base/BaseDialogFragment.kt
+++ Main/src/main/java/com/redelf/commons/activity/base/BaseDialogFragment.kt
@@ -1,6 +1,10 @@
 package com.redelf.commons.activity.base
 
+import android.app.Dialog
+import android.os.Bundle
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.DialogFragment
+import com.google.android.play.integrity.internal.ac
 import com.redelf.commons.activity.fragment.ActivityPresentable
 import com.redelf.commons.activity.fragment.FragmentWrapperActivity
 import com.redelf.commons.activity.stateful.StatefulActivity
@@ -45,4 +49,40 @@ abstract class BaseDialogFragment : DialogFragment(), ActivityPresentable {
 
         return false
     }
+
+    @Throws(IllegalArgumentException::class)
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
+        activity?.let { a ->
+
+            if (a !is BaseActivity) {
+
+                throw IllegalArgumentException(
+
+                    "Activity must be a ${BaseActivity::class.simpleName}"
+                )
+            }
+
+            val dialog = object : Dialog(a, theme) {}
+
+            dialog.setOnShowListener {
+
+                val callback = object : OnBackPressedCallback(true) {
+
+                    override fun handleOnBackPressed() {
+
+                        onBack()
+                    }
+                }
+
+                a.onBackPressedDispatcher.addCallback(a, callback)
+            }
+
+            return dialog
+        }
+
+        throw IllegalArgumentException("Activity must not be null")
+    }
+
+    protected open fun onBack() = Unit
 }
