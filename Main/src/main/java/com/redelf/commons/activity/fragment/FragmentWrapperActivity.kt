52e0c69e905195caa736333717fc14f7c71eee02
Milos Vasic <i@mvasic.ru>
2025-07-21 14:17:29 +0300

    Testing, fixing and polishing.

--- Main/src/main/java/com/redelf/commons/activity/fragment/FragmentWrapperActivity.kt
+++ Main/src/main/java/com/redelf/commons/activity/fragment/FragmentWrapperActivity.kt
@@ -11,35 +11,36 @@ import com.redelf.commons.R
 import com.redelf.commons.activity.base.BaseActivity
 import com.redelf.commons.activity.stateful.StatefulActivity
 import androidx.core.graphics.drawable.toDrawable
+import com.redelf.commons.activity.base.BaseDialogFragment
 import com.redelf.commons.activity.transition.TransitionEffectsActivity
 import com.redelf.commons.logging.Console
+import java.util.concurrent.ConcurrentHashMap
 
 
 open class FragmentWrapperActivity : BaseActivity() {
 
+    private var hash = -1
     private val tag = "Fragment Wrapper Activity ::"
 
     companion object {
 
-        const val EXTRA_FRAGMENT = "extra.fragment"
+        const val EXTRA_FRAGMENT = "fragment"
+
+        private val FRAGMENTS = ConcurrentHashMap<Int, BaseDialogFragment>()
 
         fun createIntent(
 
             context: TransitionEffectsActivity,
-            dialogFragment: DialogFragment
+            dialogFragment: BaseDialogFragment
 
         ): Intent {
 
-            return Intent(context, FragmentWrapperActivity::class.java).apply {
+            val intent = Intent(context, FragmentWrapperActivity::class.java)
+            intent.putExtra(EXTRA_FRAGMENT, dialogFragment.hashCode())
 
-                extras?.let {
+            FRAGMENTS[dialogFragment.hashCode()] = dialogFragment
 
-                    context.supportFragmentManager.putFragment(
-
-                        it, EXTRA_FRAGMENT, dialogFragment
-                    )
-                }
-            }
+            return intent
         }
     }
 
@@ -52,26 +53,28 @@ open class FragmentWrapperActivity : BaseActivity() {
 
         setContentView(R.layout.activity_fragment_wrapper)
 
-        val dialogFragment = supportFragmentManager.getFragment(
+        hash = intent.getIntExtra(EXTRA_FRAGMENT, -1)
 
-            intent.extras ?: Bundle(),
-            EXTRA_FRAGMENT
+        if (hash > 0) {
 
-        ) as? DialogFragment
+            val dialogFragment = FRAGMENTS[hash]
 
-        // TODO: Support this
-        //            df.setStyle(
-        //                intent.getIntExtra(EXTRA_STYLE, DialogFragment.STYLE_NORMAL),
-        //                intent.getIntExtra(EXTRA_THEME, 0)
-        //            )
+            // TODO: Support this
+            //            df.setStyle(
+            //                intent.getIntExtra(EXTRA_STYLE, DialogFragment.STYLE_NORMAL),
+            //                intent.getIntExtra(EXTRA_THEME, 0)
+            //            )
 
-        Console.log("$tag Dialog='$dialogFragment'")
+            Console.log("$tag Dialog='$dialogFragment'")
 
-        dialogFragment?.show(supportFragmentManager, "dialog_host")
+            dialogFragment?.show(supportFragmentManager, "dialog_host")
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
+
+        FRAGMENTS.remove(hash)
 
         Console.log("$tag onDestroy")
     }
