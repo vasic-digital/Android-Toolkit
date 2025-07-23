d668b253e06f0dde24391ee660fe8090c2723371
Milos Vasic <i@mvasic.ru>
2025-07-23 15:22:44 +0300

    Dismiss popup.

--- Main/src/main/java/com/redelf/commons/activity/popup/Popup.kt
+++ Main/src/main/java/com/redelf/commons/activity/popup/Popup.kt
@@ -1,11 +1,54 @@
 package com.redelf.commons.activity.popup
 
+import android.os.Bundle
 import com.redelf.commons.activity.transition.TransitionEffectsActivity
 import com.redelf.commons.extensions.recordException
+import com.redelf.commons.logging.Console
+import com.redelf.commons.obtain.OnObtain
+import java.util.concurrent.atomic.AtomicBoolean
 
 abstract class Popup : PopupFragment() {
 
-    protected open fun closePopup() {
+    var onDismissed: OnObtain<Boolean>? = null
+
+    private val instanceStateSaved = AtomicBoolean()
+
+    fun readyToDismiss() = !isDetached &&
+            activity?.supportFragmentManager?.isDestroyed == false &&
+            isVisible &&
+            !instanceStateSaved.get()
+
+    override fun dismiss() {
+
+        onDismissed?.let {
+
+            Console.log("$tag Triggering the callback")
+
+            it.onCompleted(getDismissResult())
+        }
+
+        if (readyToDismiss()) {
+
+            super.dismiss()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+
+        instanceStateSaved.set(true)
+
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onViewStateRestored(savedInstanceState: Bundle?) {
+        super.onViewStateRestored(savedInstanceState)
+
+        instanceStateSaved.set(false)
+    }
+
+    protected open fun getDismissResult() = true
+
+    protected open fun closePopup(from: String) {
 
         dismiss()
 
@@ -15,7 +58,7 @@ abstract class Popup : PopupFragment() {
 
                 if (it is TransitionEffectsActivity) {
 
-                    it.finishFrom("Popup.Close")
+                    it.finishFrom("Popup.Close(from='$from')")
 
                 } else {
 
