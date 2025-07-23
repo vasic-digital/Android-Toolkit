dc2ae58fe84eb65c789f8e84570c0be823a2d578
Milos Vasic <i@mvasic.ru>
2025-07-23 14:59:08 +0300

    Reset state.

--- Main/src/main/java/com/redelf/commons/stateful/flag/FlagStates.kt
+++ Main/src/main/java/com/redelf/commons/stateful/flag/FlagStates.kt
@@ -1,6 +1,7 @@
 package com.redelf.commons.stateful.flag
 
 import java.util.concurrent.ConcurrentHashMap
+import kotlin.jvm.Throws
 
 object FlagStates {
 
@@ -12,5 +13,26 @@ object FlagStates {
 
             FlagState(name)
         }
+    }
+
+    fun removeState(name: String): Boolean {
+
+        if (STATES.contains(name)) {
+
+            return STATES.remove(name) != null
+        }
+
+        return true
+    }
+
+    @Throws(IllegalStateException::class)
+    fun resetState(name: String): FlagState {
+
+        if (removeState(name)) {
+
+            return getState(name)
+        }
+
+        throw IllegalStateException("Failed to reset '$name' state")
     }
 }
