b1e5958f1be5a3ff41fd0da23270f94444824a78
Milos Vasic <i@mvasic.ru>
2025-08-07 14:57:16 +0300

    Extensiosn.

--- Main/src/main/java/com/redelf/commons/extensions/collections.kt
+++ Main/src/main/java/com/redelf/commons/extensions/collections.kt
@@ -13,10 +13,26 @@ fun <K, V> ConcurrentHashMap<K, V>.removeByValue(value: V): Boolean {
         if (entry.value == value) {
 
             iterator.remove()
-
             return true
         }
     }
 
     return false
+}
+
+fun <K, V> ConcurrentHashMap<K, V>.removeAllByValue(value: V): Boolean {
+
+    var removed = false
+    val iterator = this.iterator()
+
+    while (iterator.hasNext()) {
+
+        if (iterator.next().value == value) {
+
+            iterator.remove()
+            removed = true
+        }
+    }
+
+    return removed
 }
