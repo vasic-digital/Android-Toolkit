2fde67395268cf715c54d35477ff784a5ba0d2e6
Milos Vasic <i@mvasic.ru>
2025-08-18 16:05:03 +0300

    Small cleanup.

--- Media/src/main/java/com/redelf/commons/media/player/wrapped/ExoPlayerWorkManagerDataSourceFactory.kt
+++ Media/src/main/java/com/redelf/commons/media/player/wrapped/ExoPlayerWorkManagerDataSourceFactory.kt
@@ -1,47 +1,48 @@
 package com.redelf.commons.media.player.wrapped
 
-import android.os.Build
 import androidx.media3.common.util.UnstableApi
 import androidx.media3.datasource.DataSource
-import androidx.media3.datasource.DefaultHttpDataSource
+
+        // import android.os.Build
+        // import androidx.media3.datasource.DefaultHttpDataSource
 
 @UnstableApi
 class ExoPlayerWorkManagerDataSourceFactory : DataSource.Factory {
 
-    //            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.TIRAMISU) {
-    //
-    //            DefaultHttpDataSource.Factory()
-    //                .setConnectTimeoutMs(15000)
-    //                .setReadTimeoutMs(30000)
-    //                .setAllowCrossProtocolRedirects(true)
-    //                .setDefaultRequestProperties(
-    //
-    //                    mapOf(
-    //
-    //                        "User-Agent" to "ExoPlayer",
-    //                        "Cache-Control" to "max-stale=3600"
-    //                    )
-    //                )
-    //
-    //        } else {
-    //
-    //            ExoPlayerWorkManagerDataSourceFactory()
-    //        }
-    //
-    //    val httpDataSourceFactory = DefaultHttpDataSource.Factory()
-    //        .setConnectTimeoutMs(30_000)               // Extended timeout for Doze
-    //        .setReadTimeoutMs(60_000)                  // Longer read timeout
-    //        .setAllowCrossProtocolRedirects(true)
-    //        .setDefaultRequestProperties(
-    //
-    //            mapOf(
-    //
-    //                "User-Agent" to "ExoPlayer",
-    //                "Cache-Control" to "max-stale=3600" // Cache-friendly
-    //            )
-    //        )
-    //
-    //    */
+        //            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.TIRAMISU) {
+        //
+        //            DefaultHttpDataSource.Factory()
+        //                .setConnectTimeoutMs(15000)
+        //                .setReadTimeoutMs(30000)
+        //                .setAllowCrossProtocolRedirects(true)
+        //                .setDefaultRequestProperties(
+        //
+        //                    mapOf(
+        //
+        //                        "User-Agent" to "ExoPlayer",
+        //                        "Cache-Control" to "max-stale=3600"
+        //                    )
+        //                )
+        //
+        //        } else {
+        //
+        //            ExoPlayerWorkManagerDataSourceFactory()
+        //        }
+        //
+        //    val httpDataSourceFactory = DefaultHttpDataSource.Factory()
+        //        .setConnectTimeoutMs(30_000)               // Extended timeout for Doze
+        //        .setReadTimeoutMs(60_000)                  // Longer read timeout
+        //        .setAllowCrossProtocolRedirects(true)
+        //        .setDefaultRequestProperties(
+        //
+        //            mapOf(
+        //
+        //                "User-Agent" to "ExoPlayer",
+        //                "Cache-Control" to "max-stale=3600" // Cache-friendly
+        //            )
+        //        )
+        //
+        //    */
 
     override fun createDataSource(): DataSource {
 
@@ -64,6 +65,7 @@ class ExoPlayerWorkManagerDataSourceFactory : DataSource.Factory {
             .setAllowCrossProtocolRedirects(true)
             .setDefaultRequestProperties(cacheParameters)
             .createDataSource()
+
         //        }
     }
 }
