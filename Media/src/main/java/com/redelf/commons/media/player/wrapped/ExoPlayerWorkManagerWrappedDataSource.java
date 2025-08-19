4373ab603d93e132664398c30ca5299cc5123519
Milos Vasic <i@mvasic.ru>
2025-08-19 12:26:05 +0300

    Cooldown time updated.

--- Media/src/main/java/com/redelf/commons/media/player/wrapped/ExoPlayerWorkManagerWrappedDataSource.java
+++ Media/src/main/java/com/redelf/commons/media/player/wrapped/ExoPlayerWorkManagerWrappedDataSource.java
@@ -957,7 +957,7 @@ public class ExoPlayerWorkManagerWrappedDataSource extends BaseDataSource implem
 
                 while (
 
-                        System.currentTimeMillis() - now <= 30_000 &&
+                        System.currentTimeMillis() - now <= (2 * 60_000) &&
                                 (connection == null || connection.getInputStream() == null)
 
                 ) {}
