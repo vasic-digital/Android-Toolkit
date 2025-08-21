57d1bed8e29ec9932574e5caf4963e7023628e63
Milos Vasic <i@mvasic.ru>
2025-08-21 12:33:06 +0300

    Is in foreground?

---
+++ Main/src/main/java/com/redelf/commons/extensions/context.kt
@@ -0,0 +1,16 @@
+package com.redelf.commons.extensions
+
+import android.app.ActivityManager
+import android.content.Context
+
+fun Context.isInForeground(): Boolean {
+
+    val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager?
+    val runningAppProcesses = activityManager?.runningAppProcesses ?: return false
+
+    return runningAppProcesses.any { process ->
+
+        process.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND &&
+                process.processName == packageName
+    }
+}
