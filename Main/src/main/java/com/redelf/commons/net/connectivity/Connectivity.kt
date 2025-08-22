9de12e3f62738607aee016a59fa86a64c58a7bd1
Milos Vasic <i@mvasic.ru>
2025-08-22 14:42:26 +0300

    Connectivity improvements.

--- Main/src/main/java/com/redelf/commons/net/connectivity/Connectivity.kt
+++ Main/src/main/java/com/redelf/commons/net/connectivity/Connectivity.kt
@@ -1,32 +1,73 @@
 package com.redelf.commons.net.connectivity
 
 import android.content.Context
-import android.net.ConnectivityManager
+import android.os.NetworkOnMainThreadException
+import com.redelf.commons.extensions.isNotEmpty
+import com.redelf.commons.extensions.isOnMainThread
+import com.redelf.commons.extensions.recordException
+import com.redelf.commons.logging.Console
+import java.net.InetAddress
+import java.net.UnknownHostException
 
-class Connectivity : ConnectivityCheck {
+class Connectivity(private val endpoint: String = CHECK_ENDPOINT) : ConnectivityCheck {
+
+    companion object {
+
+        var CHECK_ENDPOINT = "www.google.com"
+    }
 
     private val defaultStrategy = object : ConnectivityCheck {
 
         @Suppress("DEPRECATION")
         override fun isNetworkAvailable(ctx: Context): Boolean {
 
-            val name = Context.CONNECTIVITY_SERVICE
+            val tag = "Network connectivity ::"
 
-            val connectivityManager = ctx.getSystemService(name) as ConnectivityManager?
+            try {
 
-            connectivityManager?.let {
+                if (isOnMainThread()) {
 
-                val activeNetworkInfo = it.activeNetworkInfo
-                return activeNetworkInfo != null && activeNetworkInfo.isConnected
+                    Console.error("$tag Network check on main thread not allowed")
+                    val e = NetworkOnMainThreadException()
+                    recordException(e)
+
+                    return false
+                }
+
+                val address = InetAddress.getByName(endpoint)
+                val online = isNotEmpty(address.toString())
+
+                if (online) {
+
+                    Console.log("$tag Online")
+
+                } else {
+
+                    Console.warning("$tag Offline")
+                }
+
+                return online
+
+            } catch (e: UnknownHostException) {
+
+                Console.warning("$tag Offline :: Error (1) = '${e.message}'")
+
+                return false
+
+            } catch (e: Throwable) {
+
+                Console.warning("$tag Offline :: Error (2) = '${e.message}'")
+
+                return false
             }
-
-            return false
         }
     }
 
     private var checkStrategy: ConnectivityCheck = defaultStrategy
 
     override fun isNetworkAvailable(ctx: Context) = checkStrategy.isNetworkAvailable(ctx)
+
+    fun isNetworkUnavailable(ctx: Context) = !isNetworkAvailable(ctx)
 
     fun setConnectivityCheckStrategy(strategy: ConnectivityCheck) {
 
