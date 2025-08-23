500b35e34ba805c059f59a6367d488be4f608a12
Milos Vasic <i@mvasic.ru>
2025-08-23 14:03:21 +0300

    The 'Safe' Recycler View.

---
+++ Main/src/main/java/com/redelf/commons/ui/SafeRecyclerView.kt
@@ -0,0 +1,34 @@
+package com.redelf.commons.ui
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.RecyclerView
+import com.redelf.commons.logging.Console
+
+class SafeRecyclerView @JvmOverloads constructor(
+
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+
+) : RecyclerView(context, attrs, defStyleAttr) {
+
+    @SuppressLint("NotifyDataSetChanged")
+    override fun onLayout(changed: Boolean, l: Int, t: Int, r: Int, b: Int) {
+
+        try {
+
+            super.onLayout(changed, l, t, r, b)
+
+        } catch (e: Throwable) {
+
+            Console.error("SafeRecyclerView :: Error='${e.message}'")
+
+            post {
+
+                adapter?.notifyDataSetChanged()
+            }
+        }
+    }
+}
