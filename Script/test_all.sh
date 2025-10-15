ec07070b4611b4eb11b2849efb9a6a9c34961484
Milos Vasic <i@mvasic.ru>
2025-10-15 19:43:42 +0300

    Auto-commit.

--- Script/test_all.sh
+++ Script/test_all.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Cleanup function to uninstall apps
+cleanup() {
+    adb uninstall russian.bear.messenger || true
+    adb uninstall russian.bear.messenger.test || true
+}
+
+# Set trap to run cleanup on exit
+trap cleanup EXIT
+
 ./gradlew test && \
 timeout 600 ./gradlew connectedAndroidTest && \
 echo "Tests executed with success"
